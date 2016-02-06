e0c2b504bab9b589520caf55d6e6f3a71517a5d7
brianhankins <brian.a.hankins@gmail.com>
2016-02-06 15:51:54 -0600

    Adjusted the JS file to work with address bar hiding - again!

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 var bg = jQuery("#bg1, #bg2");
 jQuery(window).resize("resizeBackground");
 function resizeBackground() {
-    bg.height(jQuery(window).height() + 60);
+    bg.height(jQuery(window).height());
 }
 resizeBackground();
 
